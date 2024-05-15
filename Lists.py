bcf2cf6e755c2e0104e4c62e5b996b81d0184558
farheen2706 <afabdfais@gmail.com>
2024-05-15 14:22:45 +0530

    Learning about list,tuple and dictionaries in python

---
+++ Lists.py
@@ -0,0 +1,9 @@
+list1 = [1, 2.3, "farheen"]
+
+v1 = list1[2]
+
+print(v1)
+
+list1[0] = "hema" #updating the list
+
+print("Updated list", list1)
--- datatype.py
+++ datatype.py
@@ -7,3 +7,9 @@ print("Value of xyz is ---->" + xyz)
 #python can understand the data type without variable declaration
 #'+' sign denotes to concatenation, only strings can be concatenated
 #python is strict about indentation 
+
+#List: List is nothing but array we can use square [] brackets to hold list, This is Mutable → We can modify its contents
+
+#Tuple: This is similar to List but uses small bracket () to hold values ,It’s Immutable → you can’t modify original content as tuple doesn’t support assignment operator
+
+#Dictionary: We use it to store key value pair like Map in Java ,It uses curly brackets {}
