6cf365fd6d7515a539c6c3962173fa5a23b024a9
farheen2706 <afabdfais@gmail.com>
2024-05-15 14:10:35 +0530

    Learning to use multiple variables and multiple data types at once

---
+++ multipledatatype.py
@@ -0,0 +1,4 @@
+a, b, c = 1, 3.2, "farheen" #in python multiple data types can be declared in a single line, python will automatically recognize its data type.
+
+print (a,"," ,b,",",c)
+print("Data type of a is", type(a)) #use 'type' keyword to know the datatype of the stored variable
