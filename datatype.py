a7f9d9ddad1fbfb90baba39ffca4c16495ee3df6
farheen2706 <afabdfais@gmail.com>
2024-05-15 14:04:53 +0530

    Learning about variable declaration in python

---
+++ datatype.py
@@ -0,0 +1,9 @@
+a = 20
+xyz ="Farheen"
+
+print ("Value of a is --->", a)
+print("Value of xyz is ---->" + xyz)
+
+#python can understand the data type without variable declaration
+#'+' sign denotes to concatenation, only strings can be concatenated
+#python is strict about indentation 
