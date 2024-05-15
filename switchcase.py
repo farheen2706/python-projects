2f467dccb0e2bd611463c5638fd0493368083ae7
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-05-15 11:24:42 +0000

    MAking some modifications

--- switchcase.py
+++ switchcase.py
@@ -8,3 +8,15 @@ def vote(age):
             print ("You are a senior citizen")
 
 vote(18)
+
+#Method 2
+
+def vote1 (age):
+    switch = {
+        18: "Apply ID Card",
+        30: "ELigible to vote",
+        60: "Senior Citizen" }
+    
+    return switch.get (age, "Nothing")
+
+print(vote1(30))
