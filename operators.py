4716d10b22f77b33a7e8036ef6e3799990ed186f
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-05-15 09:31:27 +0000

    Learning about operators in Python

---
+++ operators.py
@@ -0,0 +1,20 @@
+#this module i am learning about operators in python
+a = 3
+b = 12
+c = "Farheen"
+d = "Fathima"
+
+#mathematical operators
+
+e = a+b
+f = b-a
+g = b/a
+h = a*b
+i = a==b #boolean operator
+j = a != b
+print (e,f,g,h,i,j)
+
+#Logical operators
+
+print (a/2 == 2 or a==b) 
+print (a/2 == 2 and a==b)
