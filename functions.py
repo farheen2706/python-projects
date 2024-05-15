eae9a64c70efc84b82e1908cadb16fd8d6281104
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-05-15 10:50:19 +0000

    LEarning about functions and parameters

---
+++ functions.py
@@ -0,0 +1,13 @@
+#simple function
+
+def simple():
+    print("Simple function")
+
+#function with parameters
+
+def simple_addition(a, b):
+    print("Sum =", a+b)
+
+
+simple()
+simple_addition(5 , 11)
