5c4fd0747a04b0f8b944e809cf370b4ee1a0db8b
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-05-15 09:49:54 +0000

    LEarning if else statement in python

---
+++ flowcontrol.py
@@ -0,0 +1,17 @@
+#using the example of age limit to vote in india is 18
+
+age = 20
+if age > 18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")
+
+#multiple if statements
+
+if age > 18:
+    print("Eligible to vote")
+elif age == 18:
+    print ("Can apply for voter id")
+else:
+    print ("Not eligible to vote")
+
