ff4bda881f2d60b9b558f0dd6eaa1c4edcd910ce
farheen2706 <afabdfais@gmail.com>
2024-05-15 14:31:55 +0530

    Learning list, tuple and dictionary manipulation

--- Lists.py
+++ Lists.py
@@ -1,3 +1,5 @@
+
+#list
 list1 = [1, 2.3, "farheen"]
 
 v1 = list1[2]
@@ -7,3 +9,17 @@ print(v1)
 list1[0] = "hema" #updating the list
 
 print("Updated list", list1)
+
+#tuple
+
+tuple1 = (2.2,6, "hello world")
+
+print(tuple1[2]) # to print always use square brackets
+
+#dictionary (Key: Value)
+
+dictionary1 = {"Name" : "Farheen", "ID": 2 , "S.no": 45}
+
+print(dictionary1)
+print(dictionary1["Name"])#can only print using key
+
