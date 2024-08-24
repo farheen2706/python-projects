8a6edeb4b46b05c95576669db55f4b9cc7e3728e
farheen2706 <54413405+farheen2706@users.noreply.github.com>
2024-08-24 18:55:27 +0530

    Update passcrack.py

--- passcrack.py
+++ passcrack.py
@@ -4,8 +4,9 @@ print("**************PASSWORD CRACKER ******************")#display line
 
 pass_found = 0 
 
-input_hash = input("Enter the hashed password:")
-pass_doc = input("\nEnter passwords filename including path(root / home/):")
+input_hash = input("Enter the hashed password:")#getting the hashed password as input and storing it in the variable
+pass_doc = input("\nEnter passwords filename including path(root / home/):")#stores the path of he passwords file name
+
 try:
  pass_file = open(pass_doc, 'r')
  
@@ -14,14 +15,15 @@ except:
  print("Error:")
  
  print(pass_doc, "is not found.\nPlease give the path of file correctly.")
- quit()
-for word in pass_file:
+ quit()#closes the program in case of errors for error handling
+
+for word in pass_file:#reads all the values in the file
  #print(word)
- enc_word = word.encode('utf-8')
- hash_word = hashlib.md5(enc_word.strip())
- digest = hash_word.hexdigest()
+ enc_word = word.encode('utf-8')#converting to encoded
+ hash_word = hashlib.md5(enc_word.strip())#hashing the value in this function
+ digest = hash_word.hexdigest()#converts into hexadecimal string
  #print(digest)
- if digest == input_hash:
+ if digest == input_hash:#compares the values from the file and finds the password
      print("Password found.\nThe password is:", word)
  pass_found = 1
 
